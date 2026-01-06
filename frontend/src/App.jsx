435daa1b93a6c0936711719520512a7deedbccf7
raghab-pandit01 <raghab.pandit@w3developers.com.np>
2026-01-06 16:02:26 +0545

    Setup React & Tailwind

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+const App= () =>{
+
+  return (
+    <div className='bg-white min-h-screen w-full flex items-center justify-center'>
+    </div>
+  )
+}
+
+export default App
